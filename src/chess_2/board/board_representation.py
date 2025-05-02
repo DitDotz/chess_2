9b645250e23a84edb8f1ec1af3dc6600c6105d24
zhiquan <zhiquan96@gmail.com>
2025-05-02 15:29:45 +0100

    create piece repr in board_representation

--- src/chess_2/board/board_representation.py
+++ src/chess_2/board/board_representation.py
@@ -1,11 +1,28 @@
 from chess_2.utils.enums import Color, PieceType
 
+PIECE_REPR: dict[PieceType, tuple[str, str]] = {
+    PieceType.EMPTY: (" ", " "),
+    PieceType.PAWN: ("♙", "♟"),
+    PieceType.ROOK: ("♖", "♜"),
+    PieceType.BISHOP: ("♗", "♝"),
+    PieceType.QUEEN: ("♕", "♛"),
+    PieceType.KING: ("♔", "♚"),
+    PieceType.KNIGHT: ("♘", "♞"),
+}
 
-def piece_repr(self) -> str:
+
+def get_piece_repr(color:Color, piece_type:PieceType) -> str:
     """
     Get the string representation of the piece.
 
     Returns:
         str: The string representation of the piece based on its color and type.
     """
-    pass
+    if piece_type == PieceType.EMPTY or color == Color.NONE:
+        return PIECE_REPR[PieceType.EMPTY][0]
+
+    if color == Color.WHITE:
+        return PIECE_REPR[piece_type][1]
+    
+    else:
+        return PIECE_REPR[piece_type][0]
