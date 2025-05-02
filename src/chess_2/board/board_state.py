0c7701c4d5ea4a076d59033c835d2329e5a9552b
zhiquan <zhiquan96@gmail.com>
2025-05-02 16:11:08 +0100

    fix set_piece_location under BoardState

--- src/chess_2/board/board_state.py
+++ src/chess_2/board/board_state.py
@@ -1,25 +1,32 @@
 from typing import Optional, Dict, Tuple
 
 from chess_2.utils.enums import Color
+from chess_2.utils.types import Position
 
-from chess_2.piece import Piece
+from chess_2.piece.piece import Piece
 
 class BoardState:
 
     def __init__(self):
-        self.piece_pos: dict = {} # piece : tuple
+        self.piece_pos: dict[Position, Piece] = {}
         self.player_turn = Color.WHITE # default
         
-    def switch_player_turn(self):
+    def switch_player_turn(self)->None:
         """
         Switches player turn
         """
         self.player_turn = Color.BLACK if self.player_turn == Color.WHITE else Color.WHITE
 
     
-    def set_piece_location(self.piece_pos:dict, position: str, piece: str) -> None:
+    def set_piece_location(self, position: Position, piece: Piece) -> None:
         """
-        Sets the piece at a given position.
+        Sets the piece at a given board position.
+
+        Args:
+            position (Position): The (x, y) coordinates on the board.
+            piece (Piece): The piece to place at the given position.
         """
-        pass
+        self.piece_pos[position] = piece
+
+
 
