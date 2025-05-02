222998f0b4fcf026f5e1c3cbb870408b90a9a457
zhiquan <zhiquan96@gmail.com>
2025-05-02 14:59:52 +0100

    add piece_movement module and update placeholders for board_state

---
+++ src/chess_2/board/board_state.py
@@ -0,0 +1,18 @@
+from typing import Optional, Dict
+from utils import Color
+class BoardState:
+
+    def __init__(self):
+        self.curr_piece_loc: Dict[str, str] = {}
+        self.player_turn = # enum
+        
+    def player_turn(self):
+        """
+        Returns the current player's turn.
+        """
+        return self.player_turn
+    
+
+
+
+
